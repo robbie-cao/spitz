27f5e71151a51a2ba09d57b1c0b34928c9e017e4
Robbie Cao <robbie.cao@gmail.com>
2016-12-19 16:10:10 +0800

    Add queue for upload and use seperate mutex for download and upload
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- main.c
+++ main.c
@@ -15,7 +15,8 @@
 static char buf[BUFFER_SIZE];
 
 pthread_t thread_stdin, thread_download, thread_upload;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_download = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_upload = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cv_download = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cv_upload = PTHREAD_COND_INITIALIZER;
 
@@ -25,7 +26,7 @@ struct entry {
     char data[BUFFER_SIZE];
 };
 
-TAILQ_HEAD(tailhead, entry) head;
+TAILQ_HEAD(tailhead, entry) head_dq, head_uq;
 
 void thread_stdin_handler(int *arg)
 {
@@ -33,19 +34,21 @@ void thread_stdin_handler(int *arg)
     struct entry *item;
 
     fd_set readfds;
-    FD_ZERO(&readfds);
 
-    struct timeval timeout;
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 0;
+    (void) arg;     // Make compiler happy
 
     fprintf(stdout, "-> T - Input\n");
-    while (1) {
-        FD_SET(STDIN_FILENO, &readfds);
 
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    while (1) {
         if (select(1, &readfds, NULL, NULL, NULL)) {
-            scanf("%s", buf);
-            LOGD(LOG_TAG, "%s\n", buf);
+            int ret = scanf("%s", buf);
+
+            if (ret) {
+                LOGD(LOG_TAG, "%s\n", buf);
+            }
 
             switch (buf[0]) {
                 case '1':
@@ -53,18 +56,25 @@ void thread_stdin_handler(int *arg)
                         LOGD(LOG_TAG, "Wakeup T1\n");
                         item = malloc(sizeof(*item));
                         strncpy(item->data, buf, BUFFER_SIZE);
-                        LOGD(LOG_TAG, "Add to queue - item: %s\n", item->data);
-                        pthread_mutex_lock(&mutex);
-                        TAILQ_INSERT_TAIL(&head, item, entries);
+                        LOGD(LOG_TAG, "Add to dl queue - item: %s\n", item->data);
+                        pthread_mutex_lock(&mutex_download);
+                        TAILQ_INSERT_TAIL(&head_dq, item, entries);
                         pthread_cond_signal(&cv_download);
-                        pthread_mutex_unlock(&mutex);
+                        pthread_mutex_unlock(&mutex_download);
                     }
                     break;
                 case '2':
                     LOGD(LOG_TAG, "Wakeup T2\n");
-                    pthread_mutex_lock(&mutex);
-                    pthread_cond_signal(&cv_upload);
-                    pthread_mutex_unlock(&mutex);
+                    if (buf[1] == '2') {
+                        LOGD(LOG_TAG, "Wakeup T2\n");
+                        item = malloc(sizeof(*item));
+                        strncpy(item->data, buf, BUFFER_SIZE);
+                        LOGD(LOG_TAG, "Add to ul queue - item: %s\n", item->data);
+                        pthread_mutex_lock(&mutex_upload);
+                        TAILQ_INSERT_TAIL(&head_uq, item, entries);
+                        pthread_cond_signal(&cv_upload);
+                        pthread_mutex_unlock(&mutex_upload);
+                    }
                     break;
                 default:
                     break;
@@ -81,23 +91,25 @@ void thread_download_handler(int *arg)
     const char *LOG_TAG = "DL";
     struct entry *item;
 
+    (void) arg;     // Make compiler happy
+
     fprintf(stdout, "-> T - Download\n");
     pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
     while (1) {
-        pthread_mutex_lock(&mutex);
-        if (TAILQ_EMPTY(&head)) {
+        pthread_mutex_lock(&mutex_download);
+        if (TAILQ_EMPTY(&head_dq)) {
             LOGD(LOG_TAG, "Sleeping\n");
-            pthread_cond_wait(&cv_download, &mutex);
+            pthread_cond_wait(&cv_download, &mutex_download);
         }
         LOGD(LOG_TAG, "Wakeup\n");
-        item = TAILQ_FIRST(&head);
-        LOGD(LOG_TAG, "Retrieve from queue - item: %s\n", item->data);
-        TAILQ_REMOVE(&head, head.tqh_first, entries);
+        item = TAILQ_FIRST(&head_dq);
+        LOGD(LOG_TAG, "Retrieve from dl queue - item: %s\n", item->data);
+        TAILQ_REMOVE(&head_dq, head_dq.tqh_first, entries);
         free(item);
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex_download);
 
         // TODO
-        sleep(5);
+        sleep(5);       // Simulate time for download
     }
     fprintf(stdout, "<- T - Download\n");
 }
@@ -105,17 +117,27 @@ void thread_download_handler(int *arg)
 void thread_upload_handler(int *arg)
 {
     const char *LOG_TAG = "UL";
+    struct entry *item;
+
+    (void) arg;     // Make compiler happy
+
     fprintf(stdout, "-> T - Upload\n");
     pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
     while (1) {
-        pthread_mutex_lock(&mutex);
-        LOGD(LOG_TAG, "Sleeping\n");
-        pthread_cond_wait(&cv_upload, &mutex);
+        pthread_mutex_lock(&mutex_upload);
+        if (TAILQ_EMPTY(&head_uq)) {
+            LOGD(LOG_TAG, "Sleeping\n");
+            pthread_cond_wait(&cv_upload, &mutex_upload);
+        }
         LOGD(LOG_TAG, "Wakeup\n");
-        memset(buf, 0, sizeof(buf));
-        pthread_mutex_unlock(&mutex);
+        item = TAILQ_FIRST(&head_uq);
+        LOGD(LOG_TAG, "Retrieve from ul queue - item: %s\n", item->data);
+        TAILQ_REMOVE(&head_uq, head_uq.tqh_first, entries);
+        free(item);
+        pthread_mutex_unlock(&mutex_upload);
 
         // TODO
+        sleep(5);       // Simulate time for upload
     }
     fprintf(stdout, "<- T - Upload\n");
 }
@@ -124,7 +146,8 @@ int main (void)
 {
     fprintf(stdout, "Welcome - %s %s\n", __DATE__, __TIME__);
 
-    TAILQ_INIT(&head);                      /*  Initialize the queue. */
+    TAILQ_INIT(&head_dq);                   /*  Initialize the queue. */
+    TAILQ_INIT(&head_uq);                   /*  Initialize the queue. */
 
     pthread_create(&thread_stdin,
             NULL,
